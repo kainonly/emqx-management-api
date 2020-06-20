3dcf9e2224595f59e8bc2f97d74b732964a8a056
kain <zhangtqx@vip.qq.com>
2020-06-20 12:07:14 +0800

    完善API

--- src/Factory/ClientsFactory.php
+++ src/Factory/ClientsFactory.php
@@ -19,8 +19,8 @@ class ClientsFactory extends Factory
             'GET',
             ['clients'],
             [
-                'page' => $page,
-                'limit' => $limit
+                '_page' => $page,
+                '_limit' => $limit
             ]
         );
     }
@@ -35,6 +35,105 @@ class ClientsFactory extends Factory
         return $this->client->request(
             'GET',
             ['clients', $clientid]
+        );
+    }
+
+    /**
+     * 踢除指定客户端
+     * @param string $clientid
+     * @return Response
+     */
+    public function delete(string $clientid): Response
+    {
+        return $this->client->request(
+            'DELETE',
+            ['clients', $clientid]
+        );
+    }
+
+    /**
+     * 返回指定节点下所有客户端的信息
+     * @param string $node
+     * @param int|null $page 页码
+     * @param int|null $limit 每页显示的数据条数
+     * @return Response
+     */
+    public function listsForNodes(string $node, ?int $page = null, ?int $limit = null): Response
+    {
+        return $this->client->request(
+            'GET',
+            ['nodes', $node, 'clients'],
+            [
+                '_page' => $page,
+                '_limit' => $limit
+            ]
+        );
+    }
+
+    /**
+     * 返回指定节点下指定客户端的信息
+     * @param string $node
+     * @param string $clientid
+     * @return Response
+     */
+    public function getForNodes(string $node, string $clientid): Response
+    {
+        return $this->client->request(
+            'GET',
+            ['nodes', $node, 'clients', $clientid]
+        );
+    }
+
+    /**
+     * 通过 Username 查询客户端的信息
+     * @param string $username
+     * @return Response
+     */
+    public function listsOfUsername(string $username): Response
+    {
+        return $this->client->request(
+            'GET',
+            ['clients', 'username', $username]
+        );
+    }
+
+    /**
+     * 在指定节点下，通过 Username 查询指定客户端的信息
+     * @param string $node
+     * @param string $username
+     * @return Response
+     */
+    public function listsOfUsernameForNode(string $node, string $username): Response
+    {
+        return $this->client->request(
+            'GET',
+            ['nodes', $node, 'clients', 'username', $username]
+        );
+    }
+
+    /**
+     * 查询指定客户端的 ACL 缓存
+     * @param string $clientid
+     * @return Response
+     */
+    public function getAclCache(string $clientid): Response
+    {
+        return $this->client->request(
+            'GET',
+            ['clients', $clientid, 'acl_cache']
+        );
+    }
+
+    /**
+     * 清除指定客户端的 ACL 缓存
+     * @param string $clientid
+     * @return Response
+     */
+    public function deleteAclCache(string $clientid): Response
+    {
+        return $this->client->request(
+            'DELETE',
+            ['clients', $clientid, 'acl_cache']
         );
     }
 }
--- tests/ClientsTest.php
+++ tests/ClientsTest.php
@@ -10,7 +10,87 @@ class ClientsTest extends BaseTest
     public function testLists(): void
     {
         try {
-            $response = $this->client->authClient()->lists();
+            $response = $this->client->clients()->lists();
+            $this->assertFalse($response->isError());
+        } catch (Exception $e) {
+            $this->expectErrorMessage($e->getMessage());
+        }
+    }
+
+    public function testGet(): void
+    {
+        try {
+            $response = $this->client->clients()->get($this->clientid);
+            $this->assertFalse($response->isError());
+        } catch (Exception $e) {
+            $this->expectErrorMessage($e->getMessage());
+        }
+    }
+
+    public function testDelete(): void
+    {
+        try {
+            $response = $this->client->clients()->delete($this->clientid);
+            $this->assertFalse($response->isError());
+        } catch (Exception $e) {
+            $this->expectErrorMessage($e->getMessage());
+        }
+    }
+
+    public function testListsForNodes(): void
+    {
+        try {
+            $response = $this->client->clients()->listsForNodes($this->node);
+            $this->assertFalse($response->isError());
+        } catch (Exception $e) {
+            $this->expectErrorMessage($e->getMessage());
+        }
+    }
+
+    public function testGetForNodes(): void
+    {
+        try {
+            $response = $this->client->clients()->getForNodes($this->node, $this->clientid);
+            $this->assertFalse($response->isError());
+        } catch (Exception $e) {
+            $this->expectErrorMessage($e->getMessage());
+        }
+    }
+
+    public function testListsOfUsername(): void
+    {
+        try {
+            $response = $this->client->clients()->listsOfUsername($this->key);
+            $this->assertFalse($response->isError());
+        } catch (Exception $e) {
+            $this->expectErrorMessage($e->getMessage());
+        }
+    }
+
+    public function testListsOfUsernameForNodes(): void
+    {
+        try {
+            $response = $this->client->clients()->listsOfUsernameForNode($this->node, $this->key);
+            $this->assertFalse($response->isError());
+        } catch (Exception $e) {
+            $this->expectErrorMessage($e->getMessage());
+        }
+    }
+
+    public function testGetAclCache(): void
+    {
+        try {
+            $response = $this->client->clients()->getAclCache($this->clientid);
+            $this->assertFalse($response->isError());
+        } catch (Exception $e) {
+            $this->expectErrorMessage($e->getMessage());
+        }
+    }
+
+    public function testDeleteAclCache(): void
+    {
+        try {
+            $response = $this->client->clients()->deleteAclCache($this->clientid);
             $this->assertFalse($response->isError());
         } catch (Exception $e) {
             $this->expectErrorMessage($e->getMessage());
